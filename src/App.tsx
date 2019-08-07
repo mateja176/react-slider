db89eed3c760d380ff5d32fec8586e7b6e66e5e4
mateja <ma.petrovic@levi9.com>
2019-08-07 14:20:11 +0200

    feat: adds SimpleSlider

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
+import { range } from 'ramda';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
 
-const App: React.FC = () => {
+const CustomSlide: React.FC<{ i: number }> = ({ i, ...props }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      {...props}
+      style={{
+        height: 800,
+        background: 'lightcoral',
+        display: 'grid',
+        alignItems: 'center',
+        justifyItems: 'center',
+      }}
+    >
+      <h3>{i}</h3>
     </div>
   );
-}
+};
+
+type Settings = React.ComponentProps<typeof Slider>;
+
+const settings: Settings = {
+  dots: true,
+  infinite: true,
+};
+
+const SimpleSlider: React.FC = () => {
+  return (
+    <main
+      style={{
+        background: '#eee',
+        width: '50vw',
+      }}
+    >
+      <h2>Single Item</h2>
+      <Slider {...settings}>
+        {range(1, 4).map(i => (
+          <CustomSlide key={i} i={i} />
+        ))}
+      </Slider>
+    </main>
+  );
+};
+
+const App: React.FC = () => {
+  return <SimpleSlider />;
+};
 
 export default App;
